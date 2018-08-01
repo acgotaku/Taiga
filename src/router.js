78ea3b032992ac5a03f732bba4dc7176fb003b56
acgotaku <acgotaku311@gmail.com>
2018-08-01 21:51:22 +0900

    router redirect

--- src/router.js
+++ src/router.js
@@ -1,15 +1,12 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import i18n from './i18n'
 
 Vue.use(VueRouter)
-
-const LANGS = ['en', 'zh']
 const Home = () => import('@/pages/Home.vue')
 const NotFound = () => import('@/pages/NotFound.vue')
 const HelloWorld = () => import('@/components/HelloWorld.vue')
 
-const routes = [
+let routes = [
   {
     name: 'helloWorld',
     path: '/',
@@ -22,33 +19,21 @@ const routes = [
   }
 ]
 
+const redirectRoutes = routes.map(route => {
+  return { path: route.path, redirect: { name: route.name } }
+})
+
 routes.forEach(route => {
   route.path = '/:lang' + route.path
 })
+
+routes = routes.concat(redirectRoutes)
 
 routes.push({ name: '404', path: '*', component: NotFound })
 
 const router = new VueRouter({
   mode: 'history',
   routes
-})
-
-router.beforeEach((to, from, next) => {
-  const lang = to.params.lang
-  if (!LANGS.includes(lang)) {
-    next({
-      name: to.name,
-      params: Object.assign(to.params, {
-        lang: i18n.locale
-      }),
-      path: '/' + i18n.locale + to.path,
-      replace: true,
-      query: to.query
-    })
-  } else {
-    i18n.locale = lang
-    next()
-  }
 })
 
 export default router
